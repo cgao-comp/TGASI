0044df8a675db29c484cc399122de0435b2ca80a
cgao-comp <81541997+cgao-comp@users.noreply.github.com>
2023-01-11 00:23:40 +0800

    Delete code directory

--- code/network/Graph.py
+++
@@ -1,7 +0,0 @@
-import network.Vertex as VertexPackage
-
-
-class Graph_C:
-    vertexArray = []  # vertexArray = VertexPackage.Vertex_C[350000]  [VertexPackage.Vertex_C() for i in range(verNum)]
-    verNum = 0
-    edgeNum = 0
